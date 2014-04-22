d98a8c8eca08d88854ccd8a1336d4696c3d0e064
chunyen <r01922014@ntu.edu.tw>
2014-04-22 16:09:44 +0800

    fix bug, change api, add file

--- forest_predictor.cpp
+++ forest_predictor.cpp
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
-#include <iostream>
 #include <vector>
 #include "forest_pred.h"
 
@@ -10,10 +9,11 @@ using std::string;
 int main(int argc,char** argv) {
 	int correct = 0;
   int total = 0;
-	double *attr;
-
-  std::vector<double> feature;
-  double* feature_array = NULL;
+  
+  std::vector<int> feature_id;
+  std::vector<double> feature_value;
+  int* id_array = NULL;
+  double* value_array = NULL;
 
   std::ifstream fin;
   string istring;
@@ -24,34 +24,37 @@ int main(int argc,char** argv) {
     char *cstring, *tmp;
     int label;
     
-    cstring = new char[istring.size() + 5];
-    strcpy(cstring, istring.c_str());
-    
+    cstring = new char[istring.size() + 1];
+    strncpy(cstring, istring.c_str(), istring.size()+1);
+
     tmp =  strtok(cstring, ": ");
     label = atoi(tmp);
-
-    int counter = 1;
-    feature.clear();
+    feature_id.clear();
+    feature_value.clear();
     tmp = strtok(NULL, ": ");
 
     while(tmp != NULL) {
-      int feature_id = atoi(tmp);
-      while(counter < feature_id) {
-        feature.push_back(0.0);
-        counter++;
-      }
-
+      int id = atoi(tmp);
+      feature_id.push_back(id);
       tmp = strtok(NULL, ": ");
-      feature.push_back(atof(tmp));
-      counter++;
-      
+      feature_value.push_back(atof(tmp));
       tmp = strtok(NULL, ": ");
     }
+
     delete[] cstring;
     
-    int prediction = forest_predict(attr);
+    id_array = new int[feature_id.size()];
+    value_array = new double[feature_value.size()];
+    
+    for (unsigned i = 0; i < feature_id.size(); i++) {
+      id_array[i] = feature_id[i];
+      value_array[i] = feature_value[i];
+    }
+    int prediction = forest_predict(id_array, value_array, feature_id.size());
+    delete[] id_array;
+    delete[] value_array;
     if (prediction == label) correct++;
   }
-
+ 
 	printf("Accuracy: %d/%d = %lf%%\n", correct, total, correct*100.0/total);
 }
